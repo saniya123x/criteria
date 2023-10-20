67104016a693bb1c6340d340902b3b69b86ff2c0
NafeesathbiBM <nafeemuhammed17@gmail.com>
2023-10-20 12:42:25 +0530

    nnnn

--- users/edituserpro.php
+++ users/edituserpro.php
@@ -554,6 +554,7 @@
           $result = mysqli_query($conn,$sql);
           $row = mysqli_fetch_array($result)
             ?>
+            <input type="hidden" name="type" value="user"/>
             <input type="hidden" name="PRID"  value="<?php echo $prid ?>" />
             <input type="hidden" name="id" id="cid" />
             <input type="hidden" name="Upload" id="imageName" value="<?php echo $row["Upload"] ?>" />
