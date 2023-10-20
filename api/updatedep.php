4ef5bcb2ac7f4b96fdc3633423d2ff8743b11e3a
NafeesathbiBM <nafeemuhammed17@gmail.com>
2023-10-20 11:44:00 +0530

    created add button

--- api/updatedep.php
+++ api/updatedep.php
@@ -1,12 +1,17 @@
 <?php
 include 'dbcon.php';
-$did = $_GET["did"];
+$did = $_POST["did"];
+$user = $_POST["UserName"];
+$pass = $_POST["Password"];
+$name = $_POST["DName"];
+$email = $_POST["Email"];
 $sql1 = "UPDATE department
-SET DName = '$name', Email= '$email'
+SET DName = '$name', Email= '$email' ,UserName='$user'
 WHERE did = $did";
 
 if (mysqli_query($conn, $sql1)) {
   echo "New record updated successfully";
+  header("location:../admin/Department_table.php");
 } else {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
